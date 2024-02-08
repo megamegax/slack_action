476406d9b68fe878c069c94febeca5e01534d6af
Hunyady Mihály <mihaly.hunyady@emarsys.com>
2024-02-08 14:18:26 +0100

    fix(url): move to input

--- index.js
+++ index.js
@@ -2,34 +2,34 @@ const core = require('@actions/core');
 const axios = require('axios');
 import Message from './MessageFactory.js';
 
-const SLACK_WEBHOOK = process.env['SLACK_WEBHOOK'];
-
 async function run() {
-	try {
-		const channel = core.getInput('channel');
-		const message = core.getInput('message');
-		const userName = core.getInput('user_name');
-		const userIcon = core.getInput('user_icon');
-		const jobStatus = core.getInput('job_status');
-		const actions = core.getInput('actions');
+    try {
+        const channel = core.getInput('channel');
+        const message = core.getInput('message');
+        const userName = core.getInput('user_name');
+        const userIcon = core.getInput('user_icon');
+        const jobStatus = core.getInput('job_status');
+        const actions = core.getInput('actions');
+        const webHookUrl = core.getInput('webhook_url')
 
-		 await axios({
-			method: 'post',
-			url: `${SLACK_WEBHOOK}`,
-			data: Message.createMessage(jobStatus, channel, userName, message, userIcon, actions),
-			responseType: 'json',
-			headers: {
-				'Content-Type': 'application/json; charset=utf-8'
-			}
-    });
-    
+        let response = await axios({
+            method: 'post',
+            url: webHookUrl,
+            data: Message.createMessage(jobStatus, channel, userName, message, userIcon, actions),
+            responseType: 'json',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        });
 
-		core.setOutput('result', 'success');
-	} catch (error) {
-		console.log('error', error);
-		core.setOutput('result', 'failure');
-		core.setFailed(error.message);
-	}
+        console.log('success', response);
+        core.setOutput('result', 'success');
+    } catch (error) {
+        console.log('error', error);
+        console.log('error message', error.message);
+        core.setOutput('result', 'failure');
+        core.setFailed(error.message);
+    }
 }
 
 run();
